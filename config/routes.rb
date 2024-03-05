09ceb82d6814434303614b3d695a12db7bbc16f6
umekishimaru <riki12344649@gmail.com>
2024-03-05 09:20:52 +0900

    バグ修正:showページ拍子されないルーティングエラーでバッされないルーティングエラーデバック

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@
 
 Rails.application.routes.draw do
   get 'product/index'
-  get 'product/show'
+  get 'product/show/:id', to: 'product#show', as: 'product_show'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
