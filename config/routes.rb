89de4122ed8d76a8ba4468087b422bba80b7a51c
umekishimaru <riki12344649@gmail.com>
2024-03-08 11:50:52 +0900

    バグ修正:ルーティング修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  get '/', to: 'product/index'
+  root to: 'product#index'
   get 'product/show/:id', to: 'product#show', as: 'product_show'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
