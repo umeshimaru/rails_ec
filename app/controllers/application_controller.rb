dcc4eb47ea92fce8fa8b9bfb7fee30d6896f39d9
umekishimaru <riki12344649@gmail.com>
2024-03-11 19:44:09 +0900

    機能追加:開発環境basic認証実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,14 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  before_action :basic_auth
+
+  def basic_auth
+    if Rails.env == "development"
+      authenticate_or_request_with_http_basic do |username, password|
+        username == ENV['ADMIN'] && password == ENV['BASIC_PASSWORD']
+      end
+    end
+  end
+
 end
