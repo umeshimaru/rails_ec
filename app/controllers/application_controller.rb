351155ad4f81b2735b6fa66e6d8e5012a4195933
umekishimaru <riki12344649@gmail.com>
2024-03-12 08:31:47 +0900

    バグ修正:開発環境だけしかbasic認証を適用されない様に修正しました

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,10 +8,10 @@ class ApplicationController < ActionController::Base
       authenticate_or_request_with_http_basic do |username, password|
         username == ENV['ADMIN'] && password == ENV['BASIC_PASSWORD']
       end
-    end
-    unless Admin.exists?(permission: true)
-      Admin.create(permission: true) 
-      redirect_to admin_products_path
+      unless Admin.exists?(permission: true)
+        Admin.create(permission: true) 
+        redirect_to admin_products_path
+      end
     end
    
   end
