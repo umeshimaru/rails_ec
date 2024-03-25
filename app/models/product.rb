f81c43728b52ef2ec8d5a5c73e57cb1a67de437d
umekishimaru <riki12344649@gmail.com>
2024-03-25 17:32:07 +0900

    バグ修正:開発環境で更新したデータが本番環境したデータが本番環境で反映データが本番環境で反映され本番環境で反映されない

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Product < ApplicationRecord
-  has_one_attached :image, service: :amazon
+  has_one_attached :image
 
   scope :related_products, -> { order(created_at: :desc).limit(5) }
 
