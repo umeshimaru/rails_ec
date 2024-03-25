016f3ad9ca62d2f89647b446b4ed802c1d9f4241
umekishimaru <riki12344649@gmail.com>
2024-03-25 15:26:42 +0900

    追加:edit.htmlで商品の編集と更新ができるようにした

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,4 +1,5 @@
 class Admin::ProductsController < ApplicationController
+    before_action :edit_product, only: [:edit, :update]
   def new
   end
 
@@ -10,9 +11,23 @@ class Admin::ProductsController < ApplicationController
   end
 
   def update
-   
+   if @product.update(product_params)
+    redirect_to  admin_products_path
+   else
+    render :edit, status: :unprocessable_entity
+   end 
   end
 
   def delete
   end
+
+  private
+
+    def product_params
+      params.require(:product).permit(:name, :price,:product_code,:description,:images)
+    end
+
+    def edit_product
+      @product = Product.find(params[:id])
+    end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -4,4 +4,12 @@ class Product < ApplicationRecord
   has_one_attached :image
 
   scope :related_products, -> { order(created_at: :desc).limit(5) }
+
+  validates :name,         presence: true, uniqueness: true, length: { maximum: 255 }
+  validates :price,        presence: true, format: { with: /\A[0-9]+\z/ }, numericality: { only_integer: true }
+  validates :product_code, presence: true, uniqueness: true,length: { maximum: 255 }
+  validates :description,  presence: true
+
+
+
 end
