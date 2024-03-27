2fa08c1b3a127938a551415da19cb579db2c77dd
umekishimaru <riki12344649@gmail.com>
2024-03-28 00:52:38 +0900

    作成:helper/application_controller.rbにadmin_products_path_or_products_path?メソッド追記しshowページのlinkを動的に変更

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,9 @@ module ApplicationHelper
   def hidden_field_tag(_name, _value = nil, _options = {})
     raise 'Happiness chainではhidden_field_tagの使用を禁止しています'
   end
+
+  def admin_products_path_or_products_path?
+     admin? ?  admin_products_path : products_path
+  end
+
 end
