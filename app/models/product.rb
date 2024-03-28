66f3a9161996b9ec867196f678ce5b15c0f3073c
umekishimaru <riki12344649@gmail.com>
2024-03-28 21:25:04 +0900

    追加:admin/editページにエラーメッセージ追加

--- app/models/product.rb
+++ app/models/product.rb
@@ -6,8 +6,8 @@ class Product < ApplicationRecord
   scope :related_products, -> { order(created_at: :desc).limit(5) }
 
   validates :name,         presence: true, uniqueness: true, length: { maximum: 255 }
-  validates :price,        presence: true, format: { with: /\A[0-9]+\z/ }, numericality: { only_integer: true }
-  validates :product_code, presence: true, uniqueness: true,length: { maximum: 255 }
+  validates :price,        presence: true, numericality: { only_integer: true }
+  validates :product_code, presence: true, uniqueness: true,length: { maximum: 255 },format: { with: /\A[0-9]+\z/ }
   validates :description,  presence: true
 
 
