ed6d8c8cab489e880088a03048dc1b3bc6c1fdd5
umekishimaru <riki12344649@gmail.com>
2024-04-04 17:48:54 +0900

    追加:cart_product,customerコントローラーとルーティング作成

---
+++ app/controllers/cart_products_controller.rb
@@ -0,0 +1,10 @@
+class CartProductsController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+  def destroy
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  resources :cart_products, only: [:index, :new, :destroy]
+  get 'customers/new'
   root 'products#index'
   resources :products, only: %i[index show]
-
   namespace :admin do
     resources :products
   end
