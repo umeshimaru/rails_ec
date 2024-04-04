e35e82d6de7a9299949bd306616b1c47c5deff7e
umekishimaru <riki12344649@gmail.com>
2024-04-04 20:39:26 +0900

    追加:Cart_productsテーブルに数量を表すquantityカラム追加

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,4 +1,11 @@
 class Customer < ApplicationRecord
   has_many :cart_products
   has_many :products, through: :cart_products
+
+
+def self.new_token
+  SecureRandom.urlsafe_base64
+end
+
+
 end
---
+++ db/migrate/20240404113655_add_quantity_to_cart_product.rb
@@ -0,0 +1,5 @@
+class AddQuantityToCartProduct < ActiveRecord::Migration[7.0]
+  def change
+    add_column :cart_products, :quantity, :integer
+  end
+end
