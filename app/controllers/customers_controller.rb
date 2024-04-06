1cae81113c2427b244e844bd042be157506bccb9
umekishimaru <riki12344649@gmail.com>
2024-04-06 20:26:26 +0900

    追加:cart_productテーブルのquantityカラムにバリデーション追加

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -5,21 +5,20 @@ class CustomersController < ApplicationController
 
   def create
     if Customer.find_by(id: cookies.signed[:customer_id])
-      redirect_to customers_new_path
+      
     else
       customer = Customer.create
       cookies.permanent.signed[:customer_id] = customer.id
     end
   end
 
+private
+
+def cart_product_params
+  params.require(:cart_product).permit(:quantity)
+end 
 
 
 
 
-
-# @customer = Customer.new
-# puts params
-# if @customer.save(customer_params)
-# redirect_to  product_path(2)
-# end
 end
--- app/models/cart_product.rb
+++ app/models/cart_product.rb
@@ -1,4 +1,6 @@
 class CartProduct < ApplicationRecord
   belongs_to :product
   belongs_to :customer
+
+  validates :quantity, presence: true,  numericality: { in: 0..100 }
 end
