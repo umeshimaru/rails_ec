4c1874607f18db579f75185d84697efbf11c7905
umekishimaru <riki12344649@gmail.com>
2024-04-07 10:11:21 +0900

    追加:customerコントローラーcart_product_paramsのストロングパラメーターに独自項目を追加

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -4,18 +4,23 @@ class CustomersController < ApplicationController
   end
 
   def create
-    if Customer.find_by(id: cookies.signed[:customer_id])
-      
-    else
+    customer = Customer.find_by(id: cookies.signed[:customer_id])
+    if customer.nil?
       customer = Customer.create
       cookies.permanent.signed[:customer_id] = customer.id
+    else
+      cart_product = customer.cart_products.new(cart_product_params)
+      if cart_product.save
+        redirect_to customers_new_path
+      end
     end
   end
 
 private
 
 def cart_product_params
-  params.require(:cart_product).permit(:quantity)
+  permit_params = params.require(:cart_product).permit(:product_id,:quantity)
+  permit_params.merge(customer_id: cookies.signed[:customer_id])
 end 
 
 
