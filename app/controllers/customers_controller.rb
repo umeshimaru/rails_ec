d8c1c58081c64697350881dba83142147a3b4c7c
umekishimaru <riki12344649@gmail.com>
2024-04-09 11:02:28 +0900

    リファクタリング:Customerコントローラーのcreateアクシション削除

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -4,23 +4,6 @@ class CustomersController < ApplicationController
   end
 
   def create
-    customer = Customer.find_by(id: cookies.signed[:customer_id])
-    if customer.nil?
-      customer = Customer.create
-      cookies.permanent.signed[:customer_id] = customer.id
-      customer.cart_products.create(cart_product_params)
-      redirect_to products_path, flash: { primary: "カートに追加しました" }
-    else
-      customer.cart_products.create(cart_product_params)
-      redirect_to products_path, flash: { primary: "カートに追加しました" }
-    end
   end
-
-private
-
-def cart_product_params
-  permit_params = params.require(:cart_products).permit(:product_id,:quantity)
-  permit_params.merge(customer_id: cookies.signed[:customer_id])
-end 
 
 end
