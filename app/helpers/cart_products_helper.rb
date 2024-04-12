b29c8d022ec57ee171b2e99681f72f0f664430fa
umekishimaru <riki12344649@gmail.com>
2024-04-12 15:37:21 +0900

    追加:helper/cart_prodcutsにcalc_total_priceとcalc_total_quantity追加

--- app/helpers/cart_products_helper.rb
+++ app/helpers/cart_products_helper.rb
@@ -1,2 +1,11 @@
 module CartProductsHelper
+  def calc_total_quantity(cart_prodcuts)
+     total_quantity_array = cart_prodcuts.map{|cart_prodcut|  cart_prodcut.quantity}
+     total_quantity_array.sum
+  end 
+  
+  def calc_total_price(cart_prodcuts)
+    total_price_array = cart_prodcuts.map{|cart_prodcut|  cart_prodcut.product.price}
+    total_price_array.sum
+  end
 end
