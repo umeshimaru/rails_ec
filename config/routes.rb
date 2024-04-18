5aadfdcb0a76b7590dae4c070210de5f7d56f3a4
umekishimaru <riki12344649@gmail.com>
2024-04-18 10:14:39 +0900

    バグ修正:admin/productsに遷移しない問題デバック

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,15 @@ Rails.application.routes.draw do
   resources :carts, only: %i[new create]
   root 'products#index'
   resources :products, only: %i[index show]
+
   namespace :admin do
     resources :products
   end
+
+  namespace :admin do
+    resources :cart_products ,only: %i[index ]
+  end
+
+  
+ 
 end
